7eff20d706eb35513d8d1f420e59879e80400417
Alexander Ivanov <alehander42@gmail.com>
2015-12-15 02:19:24 +0200

    Add a basic ast translator

--- pseudon/ast_translator.py
+++ pseudon/ast_translator.py
@@ -1,4 +1,4 @@
-from ast import AST
+import ast
 import yaml
 
 
@@ -8,4 +8,20 @@ class ASTTranslator:
         self.tree = tree
 
     def translate(self):
-        return yaml.dump({'type': 'program', 'code': []})
+        return yaml.dump(self._translate_node(self.tree))
+
+    def _translate_node(self, node):
+        if isinstance(node, ast.AST):
+            return getattr('_translate_%s' % type(node).__name__)(**node.__dict__)
+        elif isinstance(node, list):
+            return [self._translate_node(n) for n in node]
+        elif isinstance(node, dict):
+            return {k: self._translate_node(v) for k, v in node.items()}
+        else:
+            return node
+
+    def _translate_module(self, body):
+        return {'type': 'program', 'code': self._translate_node(body)}
+
+    def _translate_int(self, n):
+        return {'type': 'int', 'value': n}
